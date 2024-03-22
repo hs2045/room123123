789aaa7d3edaa3660827b328073ec04a75293d0b
hs2045@york.ac.uk <114947312+hs2045@users.noreply.github.com>
2024-03-22 23:39:34 +0000

    fuck

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName = "Horizontal";
+    [SerializeField] private string verticalInputName = "Vertical";
+
+    [SerializeField] private float movementSpeed = 2f;
+
+    private CharacterController charController;
+
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;     
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        //通过角色控制器控制移动
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+}
